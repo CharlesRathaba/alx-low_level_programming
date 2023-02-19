37d02edad7a7eea0e4bbef0ddc9c6f294cd9336f
CharlesRathab <rathabacharles@gmail.com>
2023-02-19 03:01:21 +0200

    C program that uses puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - is our entry point
+ * Return: zero of no error, non-zero if there's an error
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}    
