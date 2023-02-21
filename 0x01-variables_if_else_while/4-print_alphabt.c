d29830c7dfd1b1d8caef84631b785338e0d11a34
CharlesRathaba <rathabacharles@gmail.com>
2023-02-21 10:33:38 -0800

    alphabets for a-z except for e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,16 +3,16 @@
  * main - entry point
  * Return: 0 (success)
  */
+
 int main(void)
 {
-    char ch;
-    for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        if (ch != 'q' && ch != 'e')
-        {
-            putchar(ch);
-        }
-    }
-    putchar('\n');
-    return (0);
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
