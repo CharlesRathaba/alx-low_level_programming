640a04191a1e4fc40fdbf7df3731d2b2c210cc96
CharlesRathab <rathabacharles@gmail.com>
2023-02-21 20:03:33 +0200

    print a-z excluding e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,9 @@ int main(void)
     for (ch = 'a'; ch <= 'z'; ch++)
     {
         if (ch != 'q' && ch != 'e')
-            {
-                putchar(ch);
-            }
+        {
+            putchar(ch);
+        }
     }
     putchar('\n');
     return (0);
