685d95dfe0c5f239ab5b4ce74809ec919955578e
CharlesRathaba <rathabacharles@gmail.com>
2023-02-27 05:39:31 -0800

    function that checks for a digit (0 through 9).

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,35 @@
+/**
+ * This is a headerfile containg declarations
+ * for prototype functions funtions define in 0x02-functions_nested_loops
+ * and 0x04-more_functions_nested_loops tasks
+ * This is like creating my own library with declared functions
+ * The tasks defines  the functions
+ */
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+void print_number(int n);
+
+#endif // MAIN_H
