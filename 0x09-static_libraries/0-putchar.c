965b3904776c8c3f9c7dcb854ccd78428a38b285
CharlesRathaba <rathabacharles@gmail.com>
2023-03-15 05:59:11 -0700

    restoring files that were deleted

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 1 on success
+ * On error, -1 is returned
+*/
+
+int _putchar(char c)
+{
+	return (fputc(c, stdout));
+}
