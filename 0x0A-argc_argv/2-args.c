496d0f9c535123c0583b06528a348bc5a8388a1f
CharlesRathaba <rathabacharles@gmail.com>
2023-03-15 07:53:57 -0700

    Program that out all the arguemnets it recieves

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments
+ * @argc: Argument count/number of argument inputed
+ * @argv: Argument vector , array of pointers to the arguments.
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s", argv[i]);
+	return (0);
+}
