c9b7a62d5d9ec5ad6a66858808ff8d5824662a61
CharlesRathaba <rathabacharles@gmail.com>
2023-04-05 19:31:43 -0700

    fix header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 
 /**
  * struct listint_s - singly linked list
- * @n - integer
+ * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node
@@ -25,11 +25,11 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(list_int **head, unsigned int idx, int n);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif LISTS_H
+#endif /* LISTS_ H*/
