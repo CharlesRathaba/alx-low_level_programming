ab2d2c639f47c7a577cb28cd0144d6eef4e29367
CharlesRathaba <rathabacharles@gmail.com>
2023-04-10 18:04:02 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	file_descripter = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	bytes_written = write(file_descripter, text_content, text_len);
 
-	if (file_decripter == -1 || bytes_written == -1)
+	if (file_descripter == -1 || bytes_written == -1)
 		return (-1);
 
 	close(file_descripter);
