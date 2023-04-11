04fa24bc472f442fdf8cd778ab18e8d713f0b424
CharlesRathaba <rathabacharles@gmail.com>
2023-04-10 17:21:53 -0700

    Reads a text file and prints it to POSIX stdout.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,9 +3,11 @@
 
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
