89d83735ee15fad3c560402079135cdbf27718c0
CharlesRathaba <rathabacharles@gmail.com>
2023-04-10 17:59:27 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (text_len = 0; text_content[text_len];)
-			len++;
+			text_len++;
 	}
 
 	file_descripter = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
